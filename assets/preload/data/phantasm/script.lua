61d65cfe746165772db4f33c70a1a5d8fa78ba03
Svusvudvudbudbudbu <101848231+Svusvudvudbudbudbu@users.noreply.github.com>
2022-03-26 17:26:32 -0600

    Add files via upload

---
+++ assets/preload/data/phantasm/script.lua
@@ -0,0 +1,33 @@
+local allowCountdown = false
+function onStartCountdown()
+	-- Block the first countdown and start a timer of 0.8 seconds to play the dialogue
+	if not allowCountdown and isStoryMode and not seenCutscene then
+		setProperty('inCutscene', true);
+		runTimer('startDialogue', 0.8);
+		allowCountdown = true;
+		return Function_Stop;
+	end
+	return Function_Continue;
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'startDialogue' then -- Timer completed, play dialogue
+		startDialogue('dialogue','city_ambience');
+	end
+end
+
+-- Dialogue (When a dialogue is finished, it calls startCountdown again)
+function onNextDialogue(count)
+	-- triggered when the next dialogue line starts, 'line' starts with 1
+end
+
+function onSkipDialogue(count)
+	-- triggered when you press Enter and skip a dialogue line that was still being typed, dialogue line starts with 1
+end
+
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'PlayableGFV2'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'deathGFV2'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --put in mods/music/
+end
